d509dfda48c919c8724f3f5d2a5c917f89e0c2c8
aksihombing <31894572+aksihombing@users.noreply.github.com>
2017-10-28 19:38:17 -0400

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,20 @@
+var i=[0];
+var images =[
+"https://image.prntscr.com/image/NUJqZtR.png",      "https://image.prntscr.com/image/hDeeyG9DStmhjhQbeFouEg.jpg",
+"https://image.prntscr.com/image/fS36DJm8Qb29joEzcLGdAw.jpg",
+"https://image.prntscr.com/image/Q0YauOezQ2WktaRE2-s_TA.jpg",
+"https://image.prntscr.com/image/zEL-1XpfQmWiV7bhoGfa8A.jpg",
+"https://image.prntscr.com/image/NtTOdE1MTFemmdJY178Ggg.jpg",
+"https://image.prntscr.com/image/TfRo0htRQCGvK6HSdMJG_w.jpg",
+"https://image.prntscr.com/image/CDup9UaMTIK3yOcBI4jZ7A.jpg",
+ 
+]
+$("#slideshow img").attr("src", images[0]);
+$("#next").click(function(){
+  i = (i+1) % images.length
+  $("#slideshow img").attr("src", images[i]);
+})
+$("#prev").click(function (){
+  i = (i+1) % images.length;
+  $("#slideshow img").attr("src", images[i])
+})
